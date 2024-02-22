2355b36186257eb81345158c344f68d5ae029d6c
SuyogyaAcharya <acharya.suyogya@gmail.com>
2024-02-22 18:08:49 -0500

    Bus movement and 3d moving
    Bus movement and 3d camera

---
+++ Team10_ZombieKiller/Assets/CarController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody RB;
+    public GameObject bus_body_art;
+
+    public Transform Wheel1;
+    public Transform Wheel2;
+    public Transform Wheel3;
+    public Transform Wheel4;
+
+    public Transform Right;
+    public Transform Left;
+    public Transform Straight;
+
+    
+    
+    void Start()
+    {
+        RB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")) {
+            RB.velocity += transform.forward * 150 * Time.deltaTime;
+
+            Wheel1.Rotate(0, 0, -500 * Time.deltaTime);
+            Wheel2.Rotate(0, 0, -500 * Time.deltaTime);
+            Wheel3.Rotate(0, 0, 500 * Time.deltaTime);
+            Wheel4.Rotate(0, 0, 500 * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey("s")) {
+            RB.velocity -= transform.forward * 80 * Time.deltaTime;
+
+            Wheel1.Rotate(0, 0, 500 * Time.deltaTime);
+            Wheel2.Rotate(0, 0, 500 * Time.deltaTime);
+            Wheel3.Rotate(0, 0, -500 * Time.deltaTime);
+            Wheel4.Rotate(0, 0, -500 * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey("a") && Input.GetKey("w")) {
+            transform.Rotate(0, -30 * Time.deltaTime, 0);
+            bus_body_art.transform.rotation = Quaternion.Lerp(bus_body_art.transform.rotation, Left.rotation, 4 * Time.deltaTime);
+            RB.velocity += bus_body_art.transform.forward * 120 * Time.deltaTime;
+            RB.velocity -= transform.forward * 110 * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey("d") && Input.GetKey("w")) {
+            transform.Rotate(0, 30 * Time.deltaTime, 0);
+            bus_body_art.transform.rotation = Quaternion.Lerp(bus_body_art.transform.rotation, Right.rotation, 4 * Time.deltaTime);
+            RB.velocity += bus_body_art.transform.forward * 120 * Time.deltaTime;
+            RB.velocity -= transform.forward * 110 * Time.deltaTime;
+        }
+
+        if (!Input.GetKey("d") && !Input.GetKey("a")) {
+            bus_body_art.transform.rotation = Quaternion.Lerp(bus_body_art.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
+
+        }
+
+
+    }
+}
